589af6feb2a2cd7403cf84557f10dc1bfab52591
Hohnn <jean-bateee@hotmail.fr>
2024-09-30 18:21:59 +0200

    database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_PRIVATE_URL')
-        },
-        pool: { min: 0 }
-    }
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5931),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
+    },
+    pool: { min: 0 },
+  },
 });
