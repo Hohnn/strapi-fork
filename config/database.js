67f7693b6c65325bdde9997b31557c92abe3a1bb
Hohnn <jean-bateee@hotmail.fr>
2024-09-30 18:40:54 +0200

    remove url

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5931),
+      host: env("PGHOST", "strapi-fork.railway.internal"),
+      port: env.int("PGPORT", 5432),
       database: env("PGDATABASE", "railway"),
       user: env("PGUSER", "postgres"),
-      password: env("PGPASSWORD", "password"),
+      password: env("PGPASSWORD", "YupNvkOmeujOUhRMOeQLcKXYNvlpOInD"),
       ssl: env.bool(true),
     },
     pool: { min: 0 },
